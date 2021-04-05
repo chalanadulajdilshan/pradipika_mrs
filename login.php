998a9afed4198cf857fd1f324e8001b519852d88
DhanushaSuharshana929 <dhanusha@sublime.lk>
2021-04-05 11:34:56 +0530

    #9 login basics

--- login.php
+++ login.php
@@ -1,10 +1,13 @@
+﻿<?php
+include_once(dirname(__FILE__) . '/class/include.php');
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0,minimal-ui" />
-        <title>Annex - Responsive Bootstrap 4 Admin Dashboard</title>
+        <title>Login</title>
         <meta content="Admin Dashboard" name="description" />
         <meta content="Mannatthemes" name="author" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -19,24 +22,27 @@
         <div class="wrapper-page">
             <div class="card">
                 <div class="card-body">
-                    <h3 class="text-center mt-0 m-b-15">
-                        <a href="index.html" class="logo logo-admin"><img src="assets/images/logo.png" height="24" alt="logo" /></a>
-                    </h3>
+                    <!--                    <h3 class="text-center mt-0 m-b-15">
+                                            <a href="index.html" class="logo logo-admin"><img src="assets/images/logo.png" height="24" alt="logo" /></a>
+                                        </h3>-->
                     <div class="p-3">
-                        <form class="form-horizontal m-t-20" action="https://mannatthemes.com/annex/vertical/index.html">
+                        <form class="form-horizontal m-t-20" method="POST" id="formlogin">
                             <div class="form-group row">
-                                <div class="col-12"><input class="form-control" type="text" required="" placeholder="Username" /></div>
+                                <div class="col-12"><input class="form-control" type="text"  name="username" id="username" data-field="username" placeholder="Username" />
+                                    <div class="valid-message"></div> 
+                                </div>
+
                             </div>
                             <div class="form-group row">
-                                <div class="col-12"><input class="form-control" type="password" required="" placeholder="Password" /></div>
+                                <div class="col-12"><input class="form-control" name="password" data-field="password" id="password" type="password" placeholder="Password" /></div>
                             </div>
                             <div class="form-group row">
-                                <div class="col-12">
-                                    <div class="custom-control custom-checkbox"><input type="checkbox" class="custom-control-input" id="customCheck1" /> <label class="custom-control-label" for="customCheck1">Remember me</label></div>
+                                <div class="col-12 message" id="message">
+                                  
                                 </div>
                             </div>
                             <div class="form-group text-center row m-t-20">
-                                <div class="col-12"><button class="btn btn-danger btn-block waves-effect waves-light" type="submit">Log In</button></div>
+                                <div class="col-12"><button class="btn btn-danger btn-block waves-effect waves-light" id="login" type="submit">Log In</button></div>
                             </div>
                             <div class="form-group m-t-10 mb-0 row">
                                 <div class="col-sm-7 m-t-20">
@@ -57,6 +63,7 @@
         <script src="assets/js/bootstrap.min.js"></script>
         <script src="assets/js/modernizr.min.js"></script>
         <script src="assets/js/detect.js"></script>
+
         <script src="assets/js/fastclick.js"></script>
         <script src="assets/js/jquery.slimscroll.js"></script>
         <script src="assets/js/jquery.blockUI.js"></script>
@@ -65,7 +72,10 @@
         <script src="assets/js/jquery.scrollTo.min.js"></script>
         <!-- App js -->
         <script src="assets/js/app.js"></script>
+        <script src="assets/js/validator.min.js" type="text/javascript"></script>
+        <script src="assets/js/jquery.validate.js" type="text/javascript"></script>
+        <script src="ajax/js/login.js" type="text/javascript"></script>
     </body>
-    
+
 </html>
 
